e91d46e3b0647186a131bad22b2ea6dcbac87bb0
sbaack <s.baack@posteo.de>
2021-07-15 19:09:59 +0200

    Some cleanup
    This just improves code readability, it doesn't add or change
    functionality.
    
    I've added two new helper functions to avoid repetitive code,
    renamed some variables to make their meaning more obvious,
    and added type hints for functions.

--- entity_networks.py
+++ entity_networks.py
@@ -11,29 +11,48 @@ from itertools import combinations
 
 #define helper functions
 
-def clean_df(df):
+def clean_df(df: pd.DataFrame):
     # Make lowercase and remove trailing whitespace
     df['Entities'] = df['Entities'].str.lower().str.strip()
     # Remove newlines
     df['Entities'] = df['Entities'].replace(r'\\n', ' ', regex=True)
     return df
 
-def clean_instagram_post(text):
+
+def create_entity_list(df: pd.DataFrame, entity_type_column: str):
+    """Create entity list from dataframe and lowercase entity names."""
+    entity_list = df[entity_type_column]
+    entity_list = [entity.lower().strip() for entity in entity_list]
+    return list(dict.fromkeys(entity_list))
+
+
+def normalize_df(df: pd.DataFrame, entity_type_column: str):
+    """Prepare merging of dataframes by applying consistent format to each.
+
+    Rename 'Organization', 'Person' or 'Conspiracy' columns to 'Entities' and 
+    add new column 'type' to retain information about entity type.
+    """
+    df.rename({entity_type_column: 'Entities'}, axis=1, inplace=True)
+    df['type'] = entity_type_column
+    return df
+
+
+def clean_instagram_post(text: str):
     text = re.sub(r"@(\w+)", '', text) #remove @-handles
     text = re.sub(r"#(\w+)", '', text) #remove hashtags
     text = text.lower() #make lowercase
     return text
 
 
-def find_entities(text, list_of_entities):
-    #convert list of entities to single regex query
-    # query = '(\b%s\b)' % '|'.join(list_of_entities)
+def find_entities(text: str, list_of_entities: list[str]):
+    # convert list of entities to single regex query
     query_list = []
     for entity in list_of_entities:
         query_list.append(rf"\b{entity}\b")
     return re.findall('|'.join(query_list), text)
 
-def get_NER_network(snapshot, entity_list, full_df):
+
+def get_NER_network(snapshot: pd.DataFrame, entity_list: list[str], full_df: pd.DataFrame):
 
     '''
     get co-occurence network of listed entities in a dataframe of instagram posts
@@ -60,6 +79,7 @@ def get_NER_network(snapshot, entity_list, full_df):
 
         G.add_weighted_edges_from(weighted_edges) #add weighted edges
 
+    # Add node attributes: Type and category of the entity the node represents
     for row in combined_df.iterrows():
         try:
             G.nodes[row[1]['Entities']]['type'] = row[1]['type']
@@ -67,7 +87,8 @@ def get_NER_network(snapshot, entity_list, full_df):
         except KeyError:
             print(f"No node named {row[1]['Entities']} found, skipping...")
         
-    nx.write_gexf(G, filename + '_PERSONS_ORGANIZATIONS_weighted.gexf')
+    nx.write_gexf(G, filename + '_PERSONS_ORGANIZATIONS_CONSPIRACIES_weighted.gexf')
+
 
 #read the sql database, store as dataframe
 con = sqlite3.connect('Fabio_insta.sqlite')
@@ -77,33 +98,24 @@ con.close()
 #convert dataframe unix timestamps to datetime
 posts_df['timestamp'] = pd.to_datetime(posts_df['timestamp'], unit='s')
 
-#define entity list
-entity_df1 = pd.read_csv("Instagram_NER - ORGANIZATIONS_CLEAN.csv")[['Organization', 'Categories']]
-entity_list1 = entity_df1['Organization']
-entity_list1 = [entity.lower().strip() for entity in entity_list1]
-entity_list1 = list(dict.fromkeys(entity_list1)) 
+#create dataframes and entity lists
+df_orgs = pd.read_csv("Instagram_NER - ORGANIZATIONS_CLEAN.csv")[['Organization', 'Categories']]
+org_list = create_entity_list(df_orgs, 'Organization')
 
-entity_df = pd.read_csv("Instagram_NER - PERSONS_CLEAN.csv")[['Person', 'Categories']]
-entity_list = entity_df["Person"]
-entity_list = [entity.lower().strip() for entity in entity_list]
-entity_list = list(dict.fromkeys(entity_list)) 
+df_person = pd.read_csv("Instagram_NER - PERSONS_CLEAN.csv")[['Person', 'Categories']]
+person_list = create_entity_list(df_person, 'Person')
 
-entity_conspiracies_df = pd.read_csv("Instagram_NER - CONSPIRACIES_NAMES.csv")[['Conspiracy', 'Categories']]
-entity_list_conspiracies = entity_conspiracies_df["Conspiracy"]
-entity_list_conspiracies = [entity.lower().strip() for entity in entity_list_conspiracies]
-entity_list_conspiracies = list(dict.fromkeys(entity_list_conspiracies)) 
+df_conspiracies = pd.read_csv("Instagram_NER - CONSPIRACIES_NAMES.csv")[['Conspiracy', 'Categories']]
+conspiracies_list = create_entity_list(df_conspiracies, 'Conspiracy')
 
-entity_list_all = list(dict.fromkeys(entity_list + entity_list1 + entity_list_conspiracies))
+entity_list_all = list(dict.fromkeys(org_list + person_list + conspiracies_list))
 
-# Build combined dataframe: Rename 'Organization' and 'Person' columns to 'Entities', add new column 'type' to
-# retain the information, then combine dataframes
-entity_df1.rename({'Organization': 'Entities'}, axis=1, inplace=True)
-entity_df1['type'] = 'organizaion'
-entity_df.rename({'Person': 'Entities'}, axis=1, inplace=True)
-entity_df['type'] = 'person'
-entity_conspiracies_df.rename({'Conspiracy': 'Entities'}, axis=1, inplace=True)
-entity_conspiracies_df['type'] = 'conspiracy'
-combined_df = pd.concat([entity_df1, entity_df, entity_conspiracies_df])
+# Normalize, combine and clean dataframes. The resulting dataframe is used to add node attributes
+# in get_NER_network()
+df_orgs = normalize_df(df_orgs, 'Organization')
+df_person = normalize_df(df_person, 'Person')
+df_conspiracies = normalize_df(df_conspiracies, 'Conspiracy')
+combined_df = pd.concat([df_orgs, df_person, df_conspiracies])
 combined_df = clean_df(combined_df)
 
 #look only at data for 2020, identify quarters
